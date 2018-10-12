cda7c4f4e4f916adab4f8306fcd0f60a67f0012b
Kelterama <dude@danielstoehr.com>
2018-10-12 11:42:39 +0200

    initial commit

---
+++ module/MyCourt/src/Controller/AccountController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace MyCourt\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AccountController extends AbstractActionController
+{
+    public function accountAction()
+    {
+        return $this->editAction();
+    }
+
+    public function listAction()
+    {
+        return new ViewModel();
+    }
+
+    public function editAction()
+    {
+        return new ViewModel();
+    }
+
+    public function registerAction()
+    {
+        return new ViewModel();
+    }
+
+    public function loginAction()
+    {
+        return new ViewModel();
+    }
+
+    public function logoutAction()
+    {
+        return new ViewModel();
+    }
+
+    public function forgotUsernameAction()
+    {
+        return new ViewModel();
+    }
+
+    public function forgotPasswordAction()
+    {
+        return new ViewModel();
+    }
+
+}
